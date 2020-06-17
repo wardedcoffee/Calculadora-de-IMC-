d0ab9089513d3af67c5ccba74e9452905a91a076
Marcos_Nascimento <marcozzymandias@gmail.com>
2020-06-17 18:12:53 -0300

    included aposentado

---
+++ aposentado.js
@@ -0,0 +1,34 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 18;
+const contribuicao = 31;
+
+// condicao1 = (contribuicao > 35) || (contribuicao > 30); 
+// condicao2 = (idade > 95) || (idade > 85);
+const condicao1 = (contribuicao > 35) || (idade >= 95);
+const condicao2 = (contribuicao > 30) || (idade >= 85);
+
+// const condicao1 = (idade + contribuicao == 95);
+// const condicao2 = (idade + contribuicao == 85);
+
+if((condicao1) || (condicao2)) {
+      console.log('Silvana, você pode se aposentar!');
+} else {
+    console.log('Silvana, você ainda não pode se aposentar!');
+    } 
+
+// } else {
+// //     if(!(condicao2)) {
+//         console.log('Silvana, você ainda não pode se aposentar!')
+// //     }
+// }
+
+// const imc = peso / (altura * altura);
+
+// if(!(imc >= 30) || (imc < 29.9)) {
+//     console.log('Marcos você não está acima do peso')
+//     } else {
+//     console.log('Marcos você está acima do peso')
+// }
+
+// console.log(imc)
